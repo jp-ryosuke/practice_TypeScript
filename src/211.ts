2a55f4f2020b74ae4b35b9df65c64b59614cb483
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-09-27 22:50:07 +0900

    変数宣言の構文

--- src/211.ts
+++ src/211.ts
@@ -1,4 +1,14 @@
-const greeting = "Hello, ";
-const text = greeting + "world!";
+//const greeting = "Hello, ";
+//const text = greeting + "world!";
 //const target = greeting;
+//console.log(text);
+
+/*const greeting = "Hello, ";
+const target = "world!";
+const text = greeting + target;
+console.log(text);*/
+
+const greeting = "Hello, ",
+      target = "world!",
+      text = greeting + target;
 console.log(text);
