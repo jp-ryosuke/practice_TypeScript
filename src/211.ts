ffa774f006b4309f7d69f07c8167764f76adc933
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-07 21:56:19 +0900

    型注釈

--- src/211.ts
+++ src/211.ts
@@ -3,12 +3,22 @@
 //const target = greeting;
 //console.log(text);
 
+import { log } from "console";
+
 /*const greeting = "Hello, ";
 const target = "world!";
 const text = greeting + target;
 console.log(text);*/
 
-const greeting = "Hello, ",
+/*const greeting = "Hello, ",
       target = "world!",
       text = greeting + target;
-console.log(text);
+console.log(text);*/
+
+//型注釈
+// const 変数:型 = 式;
+
+const greeting: string = "Hello, ";
+const target: string = "world!";
+//const target: string = 123;
+console.log(greeting + target);
