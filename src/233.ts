170b8984537ad9e31d1386a47e7c8a9b66d9f784
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-09 23:11:10 +0900

    数値リテラル

---
+++ src/233.ts
@@ -0,0 +1,13 @@
+const binary = 0b1010; //2進数リテラル
+const octal = 0o755; //8進数リテラル
+const hexadecimal = 0xff; //16進数リテラル
+
+//10 495 255
+console.log(binary, octal, hexadecimal);
+
+//指数表記のリテラル
+const big = 1e8;
+const small = 4e-5;
+
+//100000000 0.00004
+console.log(big, small);
