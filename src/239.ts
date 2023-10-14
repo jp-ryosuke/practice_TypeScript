5525ffc75404ba9ee56fb8d4461b5f91be3bf02a
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-14 20:33:13 +0900

    fix

--- src/239.ts
+++ src/239.ts
@@ -3,12 +3,12 @@
 与えられた数値から文字列を作ったり、あるいはその逆をしたり
 readline という文字列を用いた例*/
 
-import { createInterface } from "readline";
+/*import { createInterface } from "readline";
 
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout
-});
+});*/
 
 /*rl.question('文字列を入力してください:', (line) => {
     //文字列が入力されるとここが実行される
@@ -16,17 +16,19 @@ const rl = createInterface({
     rl.close();
 });*/
 
-rl.question('数値を入力してください:', (line) => {
+
+//rl.question('数値を入力してください:', (line) => {
     // line が文字列であるため期待した通り 1000 を加算した結果は出力されない
     /*//1000を足して出力
     console.log(line + 1000);
     rl.close();*/
 
     // ↓ の result は型推論で変数の型を自動的に判別している
-    const result = line + 1000;
-    console.log(result);
-    rl.close();
-});
+//    const result = line + 1000;
+//    console.log(result);
+//    rl.close();
+//});
+
 
 /*+ では実際にある型と必要な型が違う場合に自動的に値の変換が行われる
 今回は数値から文字列への変換が行われ "1000" という文字列になった
