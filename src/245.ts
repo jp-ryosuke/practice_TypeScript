4563d4a81b91f7ec6a30786af3904a7607c0cb24
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-22 04:20:41 +0900

    真偽値の演算と ! 演算子

--- src/245.ts
+++ src/245.ts
@@ -13,3 +13,26 @@ console.log(f && f); //false
 console.log(t || t); //true
 console.log(t || f); //true
 console.log(f || f); //false
+
+import {createInterface} from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('数値を入力してください:', (line) => {
+
+    const num = Number(line);
+    if(0 <= num && num < 100){
+        console.log(`${num} は0以上100未満です`);
+    }else{
+        console.log(`${num} は0以上100未満ではありません`);
+    }
+
+    if(!Number.isNaN(num)){
+        console.log(num, "は NaN ではありません");
+    }
+
+    rl.close();
+});
