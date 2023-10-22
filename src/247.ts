bc241bdd12d56939cf039f8c0b12a6bdeb4ed640
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-22 22:45:45 +0900

    三項演算子

---
+++ src/247.ts
@@ -0,0 +1,24 @@
+//条件演算子 3項演算子
+
+import { createInterface } from "readline";
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('数値を入力してください:', (line) => {
+    const num = Number(line);
+
+    const message = 0 <= num && num < 100
+    ? `${num} は0以上100未満です`
+    : `${num} は0以上100未満ではありません`;
+    
+    console.log(message);
+    rl.close();
+});
+
+/*
+
+
+*/
