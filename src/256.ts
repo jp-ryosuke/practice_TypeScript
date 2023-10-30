023de967c09c2d224f69b205ac804c3712307465
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-30 23:39:21 +0900

    for 文での continue

--- src/256.ts
+++ src/256.ts
@@ -6,3 +6,15 @@ for (let i: number = 1; i <= 100; i++) {
     summary += i;   
 }
 console.log(summary); //505
+
+/*
+for文内で continue を使用した場合更新式が実行されてから次のループに進みます
+この点は while 文にはない特殊な挙動です
+*/
+
+for(let i = 1; i <= 100; i++){
+    if(i % 2 === 1){
+        continue;
+    }
+    console.log(i); //2,4,6,...,100
+}
